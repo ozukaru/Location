ff6b681c53d035ee5c66ee4f51c3da053b03a7f6
MacBook Air <DS@MacBookOZ.local>
2017-11-10 16:07:09 -0600

    first commit

--- alwaysLocationTest/ViewController.swift
+++ alwaysLocationTest/ViewController.swift
@@ -7,19 +7,111 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    private let locationManager = CLLocationManager()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        print("\(CLLocationManager.authorizationStatus())")
+        
+            
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+                print("ALWAYS")
+                locationManager.delegate = self
+                locationManager.distanceFilter = kCLDistanceFilterNone
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.startUpdatingLocation()
+                print("\(locationManager.location)")
+            
+            }
+            else if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            print("WHEN USE")
+            locationManager.delegate = self
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            print("\(locationManager.location)")
+            locationManager.requestAlwaysAuthorization()}
+            
+            else if CLLocationManager.authorizationStatus() == .denied{
+            print("DENIED")
+            locationManager.requestAlwaysAuthorization()}
+            else {
+            print("NO")
+            locationManager.requestAlwaysAuthorization()}
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            print("viewDidAppear ALWAYS")
+            locationManager.delegate = self
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            print("\(locationManager.location)")
+            
+        }
+        else if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            print("viewDidAppear WHEN USE")
+            alertLocation()
+        }
+            
+        else if CLLocationManager.authorizationStatus() == .denied{
+            print("viewDidAppear DENIED")
+            alertLocation()
+    
+        }
+        else {
+            print("viewDidAppear NO")
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestAlwaysAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        if CLLocationManager.authorizationStatus() == .authorizedAlways {
+            print("didUpdateLocations ALWAYS")
+            locationManager.delegate = self
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            print("\(locationManager.location)")
+            
+        }
+        else if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            print("didUpdateLocations DENIED")
+            alertLocation()}
+            
+        else if CLLocationManager.authorizationStatus() == .denied{
+            print("didUpdateLocations DENIED")
+            alertLocation()}
+        else {
+            print("didUpdateLocations NO")
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.requestAlwaysAuthorization()}
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    
+    func alertLocation(){
+        var alertController = UIAlertController(title: "Es necesaria la Ubicación Siempre", message: "cambia la configuarción de localización", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "ir a Configuraciones", style: .default, handler: { action in
+            UIApplication.shared.open(URL(string:UIApplicationOpenSettingsURLString)!)}))
+        self.present(alertController, animated: true, completion: nil)
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print("\(locationManager.location)")
+       
+    }
+    
+    
+    
 }
-
